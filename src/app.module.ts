0d03bda867436704f14c26c03881083eae3546f5
Juan Sebastian Martinez <sebastianmartinez778@gmail.com>
2025-02-14 15:53:29 -0500

    backend login

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot( process.env.MONGO_URI! ),
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
