394793786b022d1ce5b0ea8d28854b5d41ef6b4f
Juan Sebastian Martinez <sebastianmartinez778@gmail.com>
2025-02-14 16:38:52 -0500

    backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot( process.env.MONGO_URI! ),
+    MongooseModule.forRoot( process.env.MONGO_URI!, {
+      dbName: process.env.MONGO_DB_NAME,
+    }),
     AuthModule,
   ],
   controllers: [],
